22079bd27c146f99e4f9cb23234cbf648af5855e
UUNagato <uunagato@gmail.com>
2017-09-08 09:46:00 +0800

    modified email and user model

--- server_src/controllers/emails.js
+++ server_src/controllers/emails.js
@@ -6,8 +6,11 @@
  */
 
 var nodemailer = require('nodemailer');
+var jwt = require('jsonwebtoken');
 var config = require('../configs/emailconfig.js');
 var validator = require('validator');
+
+const emailjwtstr = 'emancipa2ed!#';
 
 //
 // Send email
@@ -39,6 +42,35 @@ var sendfunc = async function(to, title, mainbody) {
     return true;
 };
 
+//
+// Generate Email validate token
+// param: email, user_id
+// return: token or null
+var generateValidationCodefunc = function(mail, userid) {
+    var payload = {
+        email: mail,
+        user_id: userid
+    };
+
+    var token = jwt.sign(payload, emailjwtstr, {expiresIn:'1 day'});
+    return token;
+}
+
+//
+// Validate Email validation code
+// param: token
+// return: user_id for the user or null
+var validateEmailTokenfunc = function(token) {
+    try{
+        var decoded = jwt.verify(token, emailjwtstr);
+        return decoded.user_id;
+    } catch(err) {
+        return null;
+    }
+}
+
 module.exports = {
-    send : sendfunc
+    send : sendfunc,
+    generateValidationCode : generateValidationCodefunc,
+    validateEmailToken : validateEmailTokenfunc
 };
