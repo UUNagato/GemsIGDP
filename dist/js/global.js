270be8d4f5d320af02731691523ec09c4fe83d1f
UUNagato <uunagato@gmail.com>
2017-09-15 09:10:19 +0800

    navbar v1.1

--- dist/js/global.js
+++ dist/js/global.js
@@ -1,34 +1,39 @@
 // some global operation like initialize navbar
 $().ready(function(){
-    // read login cookie
-    var cookies = document.cookie.split('; ');
-    var login = false;
-    for(i in cookies) {
-        var arr = cookies[i].split('=');
-        if(arr[0]==='authentication') {
-            login = true;
-        }
-    }
+    var login = isLocallyLogin();
 
     // check navbar
     if(login && window.localStorage) {
         // get profile
+        var userid = window.localStorage.getItem('user_id');
         var profilesrc = window.localStorage.getItem('user_profile');
         var nickname = window.localStorage.getItem('user_nickname');
-        if(profilesrc !== null && nickname !== null) {
-            changeToLoginNavbar(profilesrc, nickname);
+        if(userid !== null && profilesrc !== null && nickname !== null) {
+            changeToLoginNavbar(userid, profilesrc, nickname);
         } else {
             getUserInfoAndStore();
         }
     }
-})
+});
 
-function changeToLoginNavbar(profilesrc, nickname) {
+function isLocallyLogin() {
+    // read login cookie
+    var cookies = document.cookie.split('; ');
+    for(i in cookies) {
+        var arr = cookies[i].split('=');
+        if(arr[0]==='authentication') {
+            return true;
+        }
+    }
+    return false;
+}
+
+function changeToLoginNavbar(userid, profilesrc, nickname) {
     $('#unlog').addClass('navbar-hide');
     // set profile
-    console.log(profilesrc);
     $('#userprofile').attr('src',profilesrc);
     $('#usernick').text(nickname);
+    $('#personalpage').attr('href','/idPage/' + userid);
     $('#login').removeClass('navbar-hide');
 }
 
@@ -40,8 +45,23 @@ function getUserInfoAndStore() {
                 window.localStorage.setItem('user_id',data.user_id);
                 window.localStorage.setItem('user_profile',data.user_profile);
                 window.localStorage.setItem('user_nickname',data.user_nickname);
-                changeToLoginNavbar(data.user_profile, data.user_nickname);
+                changeToLoginNavbar(data.user_id, data.user_profile, data.user_nickname);
             }
         }
     })
+}
+
+function logOut() {
+    if(isLocallyLogin()) {
+        // clear localstorage
+        window.localStorage.removeItem('csrf');
+        window.localStorage.removeItem('user_id');
+        window.localStorage.removeItem('user_profile');
+        window.localStorage.removeItem('user_nickname');
+
+        var date = new Date();
+        date.setDate(date.getTime() - 1000);
+        document.cookie = 'authentication=null;expires=' + date;
+        location.reload();
+    }
 }
