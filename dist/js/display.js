06abc3e72f5d3cf5d6aa308806cff9556e604aad
Ryan <1041962528@qq.com>
2017-09-18 19:58:41 +0800

    隐藏按钮

---
+++ dist/js/display.js
@@ -0,0 +1,19 @@
+function onKeyDown(event){
+                    var e = event || window.event || arguments.callee.caller.arguments[0];
+                     if(e && e.keyCode==27){ // 按 Esc 
+                         //要做的事情
+                    }
+                     if(e && e.keyCode==113){ // 按 F2 
+                    //要做的事情
+                   }            
+                    if(e && e.keyCode==13){ // enter 键
+                         alert("此处回车触发搜索事件");
+                     }
+                   
+                }
+function DisplayAndHiddenBtn(btnId) {
+    if(isLocallyLogin()=="true"){
+        document.getElementById("first second").style.display="block";
+                }else
+        document.getElementById("first second").style.display="none";
+            }
