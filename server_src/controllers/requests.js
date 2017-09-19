6a88b28ee4b2b8da8b22d2387a4402e1f81d4410
hyy <hyy122116@163.com>
2017-09-19 14:51:39 +0800

    14:51 hyy

--- server_src/controllers/requests.js
+++ server_src/controllers/requests.js
@@ -7,6 +7,7 @@
 
 var models = require('../models');
 var date_convert = require('../configs/date_format.js');
+var user_control = require('/opt/gitProject/GemsIGDP/server_src/controllers/users.js');
 var Sequelize = require('sequelize');
 
 
@@ -14,7 +15,7 @@ var Sequelize = require('sequelize');
 //use for the show of all text
 var getRequestByIdfunc = async function(id) {
     var r = await models.request.findOne({
-        attributes : ['title','yuedu','release_time','content','contact'],
+        attributes : ['id','title','release_time','content','contact'],
         include: [{
             model: models.user,
             attributes : ['nickname'],
@@ -27,9 +28,10 @@ var getRequestByIdfunc = async function(id) {
     });
 
     var request = {
+        id : r.id,
         title : r.title,
-        yuedu : r.yuedu,
         release_time : date_convert.getDateTime(r.release_time),
+        author : r.user.nickname,
         content : r.content,
         contact : r.contact
     };
@@ -131,9 +133,46 @@ var getRequestListfunc = async function(currentPage){
     return requests;
 };
 
+/**
+ * 
+ * @param {integer} request_id 
+ * @return {boolean} true for delete success, false for not
+ * will throw error
+ */
+var deleteRequestfunc = async function(request_id){
+    //get current user's id
+    let user_id = user_control.getCurrentUser().user_id;
+    let request = await models.request.findOne({
+        attributes : ['user_id'],
+        where : {id : request_id}
+    });
+    
+    //if current user is this request's author, he have the authority to delete this request
+    if(user_id === request.user_id)
+    {
+        try{
+            await models.request.destroy({
+                where : {id : request_id}
+            });
+        }catch(error){
+            throw(error);
+            console.log('delete a request, errors happen:'+error);
+            return false;
+        }
+    }
+    else{
+        throw('you have no authority to delete this request!');
+        return false;
+    }
+
+    return true;
+};
+
+
 module.exports = {
     getRequestById : getRequestByIdfunc,
     releaseRequest : releaseRequestfunc,
     getRequestList : getRequestListfunc,
-    countRequests : countRequestsfunc
+    countRequests : countRequestsfunc,
+    deleteRequest : deleteRequestfunc
 }
